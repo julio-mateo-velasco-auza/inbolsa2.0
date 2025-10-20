f10d3c52db350f00bc91d2b8677c7983b7225bba
asistente sistemas <asistemas@inbolsa.com.bo>
2025-10-20 12:39:38 -0400

    sistemas

--- inbolsaNeo/bridge-privado.js
+++
@@ -1,6 +0,0 @@
-﻿(() => {
-  // Bridge mínimo para iPage: no-op seguro.
-  // Si necesitas lógica extra (auth, basePath, etc.), la añadimos luego.
-  console.debug("[bridge-privado] cargado");
-})();
-
